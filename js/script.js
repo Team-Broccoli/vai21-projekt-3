0e789708a7f43aa6d18fc3a419904a68736ae29e
racemaus <64076269+racemaus@users.noreply.github.com>
2021-05-12 15:24:56 +0300

    busshållsplatser map

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
-d3.csv('./data/fillars-2020-07.csv', function(error, data){
-    return console.log(data)
-})
+/*d3.csv('./data/fillars-2020-07.csv', (d) => {
+    //console.log(d);
+})*/
+d3.json("https://opendata.arcgis.com/datasets/c26bd38e37eb41eab20941cbe5dc6bd5_0.geojson")
+    .then(function(data){
+        let width = 500, height = 200;
+        let projection = d3.geoEqualEarth();
+        projection.fitSize([width, height], data);
+        let geoGenerator = d3.geoPath()
+            .projection(projection)
 
+        let svg = d3.select('body').append('svg')
+            .style('width', width)
+            .style('height', height);
+
+        svg.append('g').selectAll('path')
+            .data(data.features)
+            .join('path')
+            .attr('d', geoGenerator)
+            .attr('fill', 'red')
+            .attr('stroke', 'black');
+        
+    })
         
